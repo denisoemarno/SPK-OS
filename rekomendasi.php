ed015b8abcbccbd8d81dc671e30c50d9928c4ee3
denisoemarno <denisoemarno@gmail.com>
2023-12-15 18:39:32 +0700

    judul

--- rekomendasi.php
+++ rekomendasi.php
@@ -177,7 +177,7 @@
     <!-- Footer Start -->
 	<div class="footer-copyright" style="padding: 0px 0px; background-color: white">
       <div class="container">
-        <p align="center" style="color: #999">&copy; Sistem Pendukung Keputusan Pemilihan Smartphone 2018.</p>
+        <p align="center" style="color: #999">&copy; Sistem Pendukung Keputusan Pemilihan Sistem Operasi 2023.</p>
       </div>
     </div>
     <!-- Footer End -->
